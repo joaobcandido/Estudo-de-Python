b4a232ee787dfd4a8adce4c9c3b1abcb46360c89
joao candido <joan.jb56@gmail.com>
2024-07-21 20:21:16 -0300

    arithmetic operators

---
+++ 3 Operadores Aritimetricos/operadores_aritimeticos.py
@@ -0,0 +1,30 @@
+primeiro_numero = 7
+segundo_numero = 2
+
+# soma representada pelo caractere +
+soma = primeiro_numero + segundo_numero
+print(f"{primeiro_numero} + {segundo_numero} = {soma}")
+
+# subtracao representada pelo caractere -
+subtracao = primeiro_numero - segundo_numero
+print(f"{primeiro_numero} - {segundo_numero} = {subtracao}")
+
+# multiplicacao representada pelo caractere *
+multiplicacao = primeiro_numero * segundo_numero
+print(f"{primeiro_numero} x {segundo_numero} = {multiplicacao}")
+
+# divisao representada pelo caractere /
+divisao = primeiro_numero / segundo_numero
+print(f"{primeiro_numero} dividido por {segundo_numero} = {divisao}")
+
+# exponenciacao representada pelos caracteres **
+exponenciacao = primeiro_numero ** segundo_numero
+print(f"{primeiro_numero} elevado a {segundo_numero} = {exponenciacao}")
+
+# modulo (resto da divisao) representado pelos caracteres %
+modulo = primeiro_numero % segundo_numero
+print(f"o resto da divisao de {primeiro_numero} por {segundo_numero} = {modulo}")
+
+# divisao inteira representado pelos caracteres //
+modulo = primeiro_numero // segundo_numero
+print(f"divisao inteira {primeiro_numero} por {segundo_numero} = {modulo}")
