b15804c94619d0ebe14284b4e9491e7dca35b642
joao candido <joan.jb56@gmail.com>
2024-07-24 21:44:51 -0300

    if and else

---
+++ If e Else/if_else.py
@@ -0,0 +1,9 @@
+# para utilizar a estrutura condicional if fazemos assim
+numero = 21 # declaracao de uma variavel
+
+if numero == 1: # se numero for igual a 1 entao :
+    print("numero eh igual a 1") # escreva na tela numero eh igual a 1
+elif numero == 2: # se numero for igual a 2 entao :
+    print("numero eh igual a 2") # escreva na tela numero eh igual a 2
+else:
+    print("numero nao eh 1 nem 2") # escreva na tela numero nao eh 1 nem 2   
